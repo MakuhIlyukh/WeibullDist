89fc4b15345dc7025596d8a1c371ed213bfb8940
Makukha_Ilia <makukha.it@gmail.com>
2023-05-09 23:40:19 +0300

    train adepted for multiple datasets

--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 from os.path import join as joinp
 from time import perf_counter_ns
 import pickle
+import json
 
 import torch
 from torch import nn
@@ -33,7 +34,12 @@ from config import (
     TRAIN_DATA_PATH)
 
 
-TRAIN_SEED = 107
+# ALGORITHM = "gd"
+ALGORITHM = "opt_gd"
+# ALGORITHM = "manual_gd"
+# ALGORITHM = "em"
+
+START_TRAIN_SEED = 107
 LR = 10**(-1)
 N_EPOCHS = 500
 WEIGHT_DECAY = 0.0
@@ -47,61 +53,88 @@ Q_INIT = "1/m"
 
 
 if __name__ == '__main__':
+    # clearing folders
+    del_folder_content(TRAIN_PLOTS_PATH)
+
     # starting mlflow
     train_run = mlflow.start_run()
     
-    # mlflow logging
-    # params
-    mlflow.log_params({
-        "TRAIN_SEED": TRAIN_SEED,
-        "LR": LR,
-        "N_EPOCHS": N_EPOCHS,
-        "WEIGHT_DECAY": WEIGHT_DECAY,
-        "LOSS": LOSS_PREFIX,
-        "BATCH_SIZE": BATCH_SIZE})
     # commit
     set_commit_tag()
     # tag
     mlflow.set_tag(MODELS_TAG_KEY, TRAINING_TAG_VALUE)
+    
+    # mlflow logging
+    # params
+    mlflow.log_params({
+        "START_TRAIN_SEED": START_TRAIN_SEED,
+        "LR": LR,
+        "N_EPOCHS": N_EPOCHS,
+        "WEIGHT_DECAY": WEIGHT_DECAY,
+        "LOSS": LOSS_PREFIX,
+        "BATCH_SIZE": BATCH_SIZE,
+        "K_INIT": K_INIT,
+        "LMD_INIT": LMD_INIT,
+        "Q_INIT": Q_INIT,
+        "ALGORITHM": ALGORITHM
+    })
 
-    # clearing folders
-    del_folder_content(TRAIN_PLOTS_PATH)
-
-    # setting seed of torch and numpy
-    torch.manual_seed(TRAIN_SEED)
-    rnd = np.random.RandomState(TRAIN_SEED)
-
-    # loading dataset
-    # TODO: add dataset-generation-run logging
-    with open(joinp(DATASETS_ARTIFACTS_PATH, "wms.pkl"), 'rb') as f:
-        wm_sampler = WeibullMixtureSampler.load(f)
-    with open(joinp(DATASETS_ARTIFACTS_PATH, "Xy.pkl"), 'rb') as f:
-        X, y = load_dataset(f)
-    m = wm_sampler.m
+    with open(joinp(DATASETS_ARTIFACTS_PATH, "num_of_datasets.json"), "r") as f:
+        num_of_datasets = int(json.load(f))
+    with open(joinp(DATASETS_ARTIFACTS_PATH, "run_id.json"), "r") as f:
+        datasets_run_id = json.load(f)
+        mlflow.log_param("DATASETS_RUN_ID", datasets_run_id)
 
     # metrics and losses
     loss_fn = nll
     metric_fn = R2Score()
 
-    # trainer creating
-    trainer = OptimizedGD_Trainer(
-        m, opt_name="adam", lr=LR,
-        k_init=K_INIT, lmd_init=LMD_INIT, q_init=Q_INIT,
-        loss_fn=loss_fn)
-    
-    # trainer = EM_Trainer(
-    #     m, K_INIT, LMD_INIT, Q_INIT,
-    #     max_newtone_iter=5
-    # )
-    
-    mlflow.log_param("ALGORITHM", trainer.__class__)
+    results = []
+    for i in tqdm(range(num_of_datasets), position=0):
+        # setting seed of torch and numpy
+        seed = START_TRAIN_SEED + i
+        torch.manual_seed(seed)
+        rnd = np.random.RandomState(seed)
 
-    # training
-    res = trainer.train(
-        X=torch.from_numpy(X), y_true=torch.from_numpy(wm_sampler.pdf(X)),
-        n_epochs=N_EPOCHS, batch_size=BATCH_SIZE, loss_fn=loss_fn, metric_fn=metric_fn,
-        loss_prefix=LOSS_PREFIX, metric_prefix=METRIC_PREFIX
-    )
+        # loading dataset
+        # TODO: add dataset-generation-run logging
+        with open(joinp(DATASETS_ARTIFACTS_PATH, f"{i}", "wms.pkl"), 'rb') as f:
+            wm_sampler = WeibullMixtureSampler.load(f)
+        with open(joinp(DATASETS_ARTIFACTS_PATH, f"{i}", "Xy.pkl"), 'rb') as f:
+            X, y = load_dataset(f)
+        m = wm_sampler.m
+
+        # trainer creating
+        if ALGORITHM == "gd":
+            pass
+        elif ALGORITHM == "opt_gd":
+            trainer = OptimizedGD_Trainer(
+                m, opt_name="adam", lr=LR,
+                k_init=K_INIT, lmd_init=LMD_INIT, q_init=Q_INIT,
+                loss_fn=loss_fn)
+        elif ALGORITHM == "manual_gd":
+            pass
+        elif ALGORITHM == "em":
+            trainer = EM_Trainer(
+                m, K_INIT, LMD_INIT, Q_INIT,
+                max_newtone_iter=5
+            )
+        else:
+            raise ValueError(f"Unknown algorithm = {ALGORITHM}")
+
+        # training
+        res = trainer.train(
+            X=torch.from_numpy(X), y_true=torch.from_numpy(wm_sampler.pdf(X)),
+            n_epochs=N_EPOCHS, batch_size=BATCH_SIZE, loss_fn=loss_fn, metric_fn=metric_fn,
+            loss_prefix=LOSS_PREFIX, metric_prefix=METRIC_PREFIX
+        )
+        results.append({
+            "i": i,
+            "res": res
+        })
+        
+    with open(joinp(TRAIN_DATA_PATH, "train_results.pkl"), "wb") as f:
+        pickle.dump(results, f)
 
     mlflow.log_artifact(TRAIN_PLOTS_PATH)
     # mlflow.pytorch.log_model(gm, TRMP)
